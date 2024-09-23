1e03512dd8940bbaa5d89a64e4a116d7d40ee338
Caique Marques <mqcaique@gmail.com>
2024-09-22 22:08:22 -0300

    feat: admin can add, remove and edit new products

--- joestore/store/views.py
+++ joestore/store/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import *
 
-# Create your views here.
-def store(request):
-    context = {}
+def store(request) -> HttpResponse:
+    products = Product.objects.all()
+    context = {'products': products}
     return render(request, 'store/store.html', context)
 
 
-def cart(request):
+def cart(request) -> HttpResponse:
     context = {}
     return render(request, 'store/cart.html', context)
 
 
-def checkout(request):
+def checkout(request) -> HttpResponse:
     context = {}
     return render(request, 'store/checkout.html', context)
