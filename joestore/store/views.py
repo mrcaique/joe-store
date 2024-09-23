d0fb877d313ca0b6b2e9a6487cd6091434fa9ff4
Caique Marques <mqcaique@gmail.com>
2024-09-23 19:32:37 -0300

    feat: add checkout form and process order

--- joestore/store/views.py
+++ joestore/store/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from .models import *
 import json
+import datetime
 
 def store(request) -> HttpResponse:
     if request.user.is_authenticated:
@@ -77,3 +78,31 @@ def updateItem(request) -> JsonResponse:
     if orderItem.quantity <= 0:
         orderItem.delete()
     return JsonResponse('Item was added', safe=False)
+
+
+def processOrder(request) -> JsonResponse:
+    transaction_id = datetime.datetime.now().timestamp()
+    data = json.loads(request.body)
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(
+            customer=customer,
+            complete=False
+        )
+        total = float(data['form']['total'])
+        order.transaction_id = transaction_id
+        if total == order.get_cart_total:
+            order.complete = True
+        order.save()
+        if order.shipping == True:
+            ShippingAddress.objects.create(
+                customer=customer,
+                order=order,
+                address=data['shipping']['address'],
+                city=data['shipping']['city'],
+                state=data['shipping']['state'],
+                zipcode=data['shipping']['zipcode'],
+            )
+    else:
+        print('User is not logged in')
+    return JsonResponse('Payment submitted', safe=False)
